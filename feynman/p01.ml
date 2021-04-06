eef6d6687eddc67e8042ffef1b18806b73d73823
shenlebantongying <shenlebantongying@gmail.com>
2021-04-06 02:36:24 -0400

    acc + "regional acc" -> list manipulating with states

--- feynman/p01.ml
+++ feynman/p01.ml
@@ -51,3 +51,34 @@ let rec compress = function
 let%test _ = compress ["a";"a";"a";"b";"c";"c"] = ["a";"b";"c"] 
 
 (* TODO: 09 pack list into sublists *)
+
+
+let pack list = 
+    let rec aux cur acc = function
+        | [] -> []
+        | [x] -> (x::cur) :: acc
+(* Take the head each time and compare it with the next element
+if they are same, put a into the cur, and pass the unchanged acc
+else emptify the cur_list, concatenate cur_list into the accumulator*)
+        | a :: (( b :: _ ) as t) ->
+            if a = b then aux (a::cur) acc t
+            else aux [] (( a :: cur) :: acc) t in
+    List.rev (aux [] [] list)
+
+let%test _ = pack [1;1;2;2;2;3;] = [[1;1];[2;2;2];[3]]
+
+(* Using the same principle on "Run-length encoding"*)
+
+let encode list = 
+    let rec aux count acc = function
+        | []  -> []
+        | [x] -> (count+1,x) :: acc
+        | a::( b::_ as t) ->
+            if a = b then aux (count + 1) acc t
+            else aux 0 ((count+1,a)::acc) t in 
+    List.rev (aux 0 [] list);;
+
+let%test _ = 
+    encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"] 
+    =[(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")]
+
